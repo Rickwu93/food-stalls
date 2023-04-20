a1ff948af02c249182967a852dc9992ac2dc520d
Ricky Wu <rickywu209@gmail.com>
2023-04-19 23:47:45 -0600

    adding an explore menu button and more css

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,10 +9,12 @@ const Header = () => (
     <div className="app__wrapper__info">
       <SubHeading title="Chase the new flavor"/>
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0'}}>For FaZe, many might have thought that they missed their opportunity. They won three IGS events during their hegemony in the first half of 2022, but they — like many a great team before them</p>
+      <button type="button" className="custom__button">Explore Menu</button>
     </div>
 
     <div className="app__wrapper_img">
-
+      <img src={images.welcome} alt="header img" />
     </div>
   </div>
 );
