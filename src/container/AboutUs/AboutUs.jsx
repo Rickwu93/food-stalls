a23e2bb8c8ca8ff319e1e72fe6b136c57a7fd7fc
Ricky Wu <rickywu209@gmail.com>
2023-04-23 23:39:48 -0600

    adding our history section besides the about us

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -18,8 +18,16 @@ const AboutUs = () => (
       </div>
 
       <div className="app__aboutus-content_knife flex__center">
-          <img src={images.knife} />
+          <img src={images.knife} alt="about_knife" />
       </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">Mathieu "⁠ZywOo⁠" Herbaut has claimed the HLTV x 1xBet MVP award for IEM Rio following Vitality's 2-0 victory over Heroic in the grand final.</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
+
     </div>
   </div>
 );
