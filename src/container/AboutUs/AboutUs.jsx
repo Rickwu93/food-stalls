cbdb43c8c6642e75ba794a124b5a1a38574fa34b
Ricky Wu <rickywu209@gmail.com>
2023-04-23 23:23:39 -0600

    Simple about-us page with sample text and a know more button

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -10,7 +10,12 @@ const AboutUs = () => (
     </div>
 
     <div className="app__aboutus-content flex__center">
-      
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">Mathieu "⁠ZywOo⁠" Herbaut has claimed the HLTV x 1xBet MVP award for IEM Rio following Vitality's 2-0 victory over Heroic in the grand final.</p>
+        <button type="button" className="custom__button">Know More</button>
+      </div>
     </div>
   </div>
 );
