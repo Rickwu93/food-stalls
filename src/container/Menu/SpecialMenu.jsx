1233c240dc9854c71a31a6712c67f25765836dff
Ricky Wu <rickywu209@gmail.com>
2023-05-03 23:58:21 -0600

    adding in individual wine and beer section

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -11,7 +11,14 @@ const SpecialMenu = () => (
       <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
 
-    
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className="app__specialMenu_menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu_menu_items">
+          
+        </div>
+      </div>
+    </div>
   </div>
 );
 
