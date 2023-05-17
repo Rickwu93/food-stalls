aef4119250ec3bf20b48043a1633eb05352a4695
Ricky Wu <rickywu209@gmail.com>
2023-05-16 23:28:33 -0600

    adding in css for chef page

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 
 import { SubHeading } from '../../components';
-import { images } from '../..constants';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
   <div className="app__bg app__wrapper section__padding">
     <div className="app__wrapper_img app__wrapper_img-reverse">
       <img src={images.chef} alt="chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+        <SubHeading title="Chef's Word" />
+        <h1 className="headtext__cormorant">What we believe in</h1>
+
+        <div className="app__chef-content">
+          <div className="app__chef-content_quote">
+            <img src={images.quote} alt="quote" />
+            <p className="p__opensans">Finn "⁠karrigan⁠" Andersen executed some great calls and found frags in crucial moments to keep his team in the game.</p>
+          </div>
+          <p className="p__opensans">FAZE ELIMINATE NAVI AND CLINCH PARIS MAJOR PLAYOFFS SPOT</p>
+        </div>
+
+        <div className="app__chef-sign">
+          <p>Steph Curry</p>
+          <p className="p__opensans">Chef & Founder</p>
+
+          <img src={images.sign} alt="sign" />
+        </div>
+
     </div>
   </div>
 );
