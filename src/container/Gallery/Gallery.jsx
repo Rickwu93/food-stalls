e0bd92fda83974b1832e6bc99cce1793b2dc67c6
Ricky Wu <rickywu209@gmail.com>
2023-06-05 12:58:33 -0600

    adding gallery section to show food images

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+const scrollRef = React.useRef(null);
+
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading  title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: 'AAA', marginTop: '2rem' }}>
+        The Spanish AWPer claims the first Most Valuable Player medal in his career after leading ENCE to the IEM Dallas title.</p>
+        <button type="button" className="custom__button">View More</button>
+      </div>
+
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icons" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icons" onClick={() => scroll('right')} />
+        </div>
+      </div>
+    </div>
+  );
+
+}
 
 export default Gallery;
