2cc70d48b1e3b7a1426b22bee7c9c26f386b9c04
Ricky Wu <rickywu209@gmail.com>
2023-06-05 14:10:36 -0600

    food images mapped out hard coded

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -5,8 +5,22 @@ import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
 
+const galleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
+
 const Gallery = () => {
-const scrollRef = React.useRef(null);
+  const scrollRef = React.useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if(direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  }
+
+
 
   return (
     <div className="app__gallery flex__center">
@@ -20,7 +34,11 @@ const scrollRef = React.useRef(null);
 
       <div className="app__gallery-images">
         <div className="app__gallery-images_container" ref={scrollRef}>
-
+          {galleryImages.map((image, index) => (
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery" />
+            </div>
+          ))}
         </div>
         <div className="app__gallery-images_arrows">
           <BsArrowLeftShort className="gallery__arrow-icons" onClick={() => scroll('left')} />
