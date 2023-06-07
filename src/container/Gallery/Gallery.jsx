f063d5c019b76689907b62ad77000396c447c5cb
Ricky Wu <rickywu209@gmail.com>
2023-06-06 23:40:34 -0600

    arrow key scroll fix on gallery

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -5,50 +5,43 @@ import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
 
-const galleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
-
 const Gallery = () => {
   const scrollRef = React.useRef(null);
 
   const scroll = (direction) => {
     const { current } = scrollRef;
 
-    if(direction === 'left') {
+    if (direction === 'left') {
       current.scrollLeft -= 300;
     } else {
       current.scrollLeft += 300;
     }
-  }
-
-
+  };
 
   return (
     <div className="app__gallery flex__center">
       <div className="app__gallery-content">
-        <SubHeading  title="Instagram" />
+        <SubHeading title="Instagram" />
         <h1 className="headtext__cormorant">Photo Gallery</h1>
-        <p className="p__opensans" style={{ color: 'AAA', marginTop: '2rem' }}>
-        The Spanish AWPer claims the first Most Valuable Player medal in his career after leading ENCE to the IEM Dallas title.</p>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
         <button type="button" className="custom__button">View More</button>
       </div>
-
       <div className="app__gallery-images">
         <div className="app__gallery-images_container" ref={scrollRef}>
-          {galleryImages.map((image, index) => (
+          {[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
             <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
-              <img src={image} alt="gallery" />
-              <BsInstagram className="gallery__images-icon" />
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
             </div>
           ))}
         </div>
         <div className="app__gallery-images_arrows">
-          <BsArrowLeftShort className="gallery__arrow-icons" onClick={() => scroll('left')} />
-          <BsArrowRightShort className="gallery__arrow-icons" onClick={() => scroll('right')} />
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
         </div>
       </div>
     </div>
   );
-
-}
+};
 
 export default Gallery;
