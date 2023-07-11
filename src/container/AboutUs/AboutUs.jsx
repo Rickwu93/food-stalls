7f63b661fdb6c83b0b20e26a6d8a377c4d968b42
Ricky Wu <rickywu209@gmail.com>
2023-07-10 20:58:33 -0600

    fixing formatting on about us page

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import { images } from '../../constants';
-import './AboutUs.css';
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
     <div className="app__aboutus-overlay flex__center">
       <img src={images.G} alt="g letter" />
     </div>
@@ -13,21 +16,32 @@ const AboutUs = () => (
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Mathieu "⁠ZywOo⁠" Herbaut has claimed the HLTV x 1xBet MVP award for IEM Rio following Vitality's 2-0 victory over Heroic in the grand final.</p>
-        <button type="button" className="custom__button">Know More</button>
+        <p className="p__opensans">
+          Mathieu "⁠ZywOo⁠" Herbaut has claimed the HLTV x 1xBet MVP award for
+          IEM Rio following Vitality's 2-0 victory over Heroic in the grand
+          final.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
       </div>
 
       <div className="app__aboutus-content_knife flex__center">
-          <img src={images.knife} alt="about_knife" />
+        <img src={images.knife} alt="about_knife" />
       </div>
 
       <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Mathieu "⁠ZywOo⁠" Herbaut has claimed the HLTV x 1xBet MVP award for IEM Rio following Vitality's 2-0 victory over Heroic in the grand final.</p>
-        <button type="button" className="custom__button">Know More</button>
+        <p className="p__opensans">
+          Mathieu "⁠ZywOo⁠" Herbaut has claimed the HLTV x 1xBet MVP award for
+          IEM Rio following Vitality's 2-0 victory over Heroic in the grand
+          final.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
       </div>
-
     </div>
   </div>
 );
